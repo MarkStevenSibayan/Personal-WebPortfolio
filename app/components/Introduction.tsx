f36acc89114cd864a7c85b0e531b4cb8756220a2
unknown <markstevensibayan11@gmail.com>
2025-06-02 09:14:56 +0530

    Try

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -23,7 +23,7 @@ export default function Introduction() {
       </h1>
       <p className="text-lg sm:text-xl text-gray-400 mb-4">Bachelor of Science in Information Technology</p>
       <p className="max-w-2xl mx-auto text-gray-300 text-sm sm:text-base">
-        I am a 4th-year IT student passionate about technology and related skills. Skilled in programming, web
+        I am a 3rd-year IT student passionate about technology and related skills. Skilled in programming, web
         development, and communication I am eager to gain hands-on experience and grow in the IT industry.
       </p>
     </section>
